671f6b916d442f9967cd91ad98ef7554ecc3eb5c
latif-muhammad <m.latif.25120@khi.iba.edu.pk>
2025-05-10 02:39:30 +0500

    config env

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb+srv://muhammadlatifadd199:admin@cluster0.36ouufb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(() => {
   console.log("Mongo Connected")
   app.listen(PORT, () => {
